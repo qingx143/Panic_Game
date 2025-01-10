770dbd225baa5e86394e82e8ed85e5f370a4b706
xiongz42 <zi.xiong@mail.utoronto.ca>
2025-01-10 15:17:00 -0500

    added the base pygame code structure

---
+++ main.py
@@ -0,0 +1,27 @@
+import pygame
+import sys
+
+# initializing game start
+pygame.init()
+clock = pygame.time.Clock()
+
+# initializing screen
+screen_width = 800
+screen_height = 600
+screen = pygame.display.set_mode((screen_width, screen_height))
+
+running = True
+
+while running:
+    screen.fill('white')
+    mouse = pygame.mouse.get_pos()
+
+    # for loop through the event queue
+    for event in pygame.event.get():
+        # check for QUIT event
+        if event.type == pygame.QUIT:
+            running = False
+            sys.exit()
+
+    pygame.display.update()
+    clock.tick(60)
