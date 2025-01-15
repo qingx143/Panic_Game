11aecadcc1619e84e4cd6c90db5575a43339a756
Zi Qing Xiong <qingx1137@gmail.com>
2025-01-15 10:27:02 -0500

    changed screen sizes

--- main.py
+++ main.py
@@ -6,8 +6,8 @@ pygame.init()
 clock = pygame.time.Clock()
 
 # initializing screen
-screen_width = 800
-screen_height = 600
+screen_width = 1000
+screen_height = 700
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 running = True
