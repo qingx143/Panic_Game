8eb359ac3720b6ce9d436aa83c550861506ce486
Zi Qing Xiong <qingx1137@gmail.com>
2025-01-21 21:05:29 -0500

    character movement complete

--- front_house.py
+++ front_house.py
@@ -1,6 +1,6 @@
 import pygame
 
-def front_house_layout(screen):
+def front_house_layout(screen, x, y, w, h):
     screen.fill('grey')
 
     # back wall
@@ -20,3 +20,51 @@ def front_house_layout(screen):
     # tables
     pygame.draw.ellipse(screen, 'black', pygame.Rect(750, 170, 170, 140))
     pygame.draw.ellipse(screen, 'black', pygame.Rect(750, 440, 170, 140))
+
+    character_movement(screen, x, y, w, h)
+
+def character_movement(screen, player_x, player_y, player_w, player_h):
+    player = pygame.Rect(player_x, player_y, player_w, player_h)
+    pygame.draw.rect(screen, 'blue', player)
+
+def is_touching_objects(x, y, w, h) -> str:
+    # back wall
+    if is_touching(x, y, w, h, 0, 0, 1000, 25):
+        return is_touching_side(x, y, w, h, 0, 0, 1000, 25)
+
+    # counters
+    if is_touching(x, y, w, h, 0, 225, 575, 100):
+        return is_touching_side(x, y, w, h, 0, 225, 575, 100)
+    if is_touching(x, y, w, h, 0, 25, 100, 50):
+        return is_touching_side(x, y, w, h, 0, 25, 100, 50)
+    if is_touching(x, y, w, h, 250, 25, 375, 50):
+        return is_touching_side(x, y, w, h, 250, 25, 375, 50)
+
+    # tables
+    if is_touching(x, y, w, h, 750, 170, 170, 140):
+        return is_touching_side(x, y, w, h, 750, 170, 170, 140)
+    if is_touching(x, y, w, h, 750, 440, 170, 140):
+        return is_touching_side(x, y, w, h, 750, 440, 170, 140)
+
+    return "NONE"
+
+def is_touching(x, y, w, h, ox, oy, ow, oh) -> bool:
+    if (x + w >= ox) and (x <= ox + ow) and (y + h >= oy) and (y <= oy + oh):
+        return True
+    return False
+
+def is_touching_side(x, y, w, h, ox, oy, ow, oh) -> str:
+    if x + w == ox:
+        return "RIGHT"
+    if x == ox + ow:
+        return "LEFT"
+    if y + h == oy:
+        print("FDLSJFLKDJSFKLJDSKLFJKLDSJFKLDSJFKLJDSL")
+        return "TOP"
+    if y == oy + oh:
+        return "BOTTOM"
+    return "NONE"
+
+
+
+
--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 
-from front_house import front_house_layout
+import front_house
 
 # initializing game start
 pygame.init()
@@ -14,10 +14,32 @@ screen = pygame.display.set_mode((screen_width, screen_height))
 
 running = True
 
+# initiating character keys
+akey = False
+wkey = False
+skey = False
+dkey = False
+player_x = 420
+player_y = 140
+player_w = 50
+player_h = 75
+
 while running:
     screen.fill('white')
     mouse = pygame.mouse.get_pos()
-    front_house_layout(screen)
+
+    # character movement
+    if akey and not front_house.is_touching_objects(player_x, player_y, player_w, player_h) == "RIGHT":
+        player_x -= 1
+    if dkey and not front_house.is_touching_objects(player_x, player_y, player_w, player_h) == "LEFT":
+        player_x += 1
+    if wkey and not front_house.is_touching_objects(player_x, player_y, player_w, player_h) == "UP":
+        print(front_house.is_touching_objects(player_x, player_y, player_w, player_h))
+        player_y -= 1
+    if skey and not front_house.is_touching_objects(player_x, player_y, player_w, player_h) == "DOWN":
+        player_y += 1
+
+    front_house.front_house_layout(screen, player_x, player_y, player_w, player_h)
 
     # for loop through the event queue
     for event in pygame.event.get():
@@ -25,6 +47,42 @@ while running:
         if event.type == pygame.QUIT:
             running = False
             sys.exit()
+
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_a:
+                akey = True
+            if event.key == pygame.K_w:
+                wkey = True
+            if event.key == pygame.K_s:
+                skey = True
+            if event.key == pygame.K_d:
+                dkey = True
+            if event.key == pygame.K_LEFT:
+                akey = True
+            if event.key == pygame.K_UP:
+                wkey = True
+            if event.key == pygame.K_DOWN:
+                skey = True
+            if event.key == pygame.K_RIGHT:
+                dkey = True
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_a:
+                akey = False
+            if event.key == pygame.K_w:
+                wkey = False
+            if event.key == pygame.K_s:
+                skey = False
+            if event.key == pygame.K_d:
+                dkey = False
+            if event.key == pygame.K_LEFT:
+                akey = False
+            if event.key == pygame.K_UP:
+                wkey = False
+            if event.key == pygame.K_DOWN:
+                skey = False
+            if event.key == pygame.K_RIGHT:
+                dkey = False
 
     pygame.display.update()
     clock.tick(60)
